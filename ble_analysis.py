43670d5ef169c31c8e20f4fd82056d48b6f8e851
Ismaeel AlAlawi <38157688+ialalawi@users.noreply.github.com>
2021-09-11 16:05:29 -0400

    Add files via upload
    Modified code file - not yet clean code with comments, will refine further

--- ble_analysis.py
+++ ble_analysis.py
@@ -1,10 +1,22 @@
-# Analysis of BLE scanned data 
+lines_seen = set()
 
-with open('hw11_miniproj_data.txt') as f:
-    seen = set()
-    for line in f:
-        line_lower = line.lower()
-        if line_lower in seen:
-            print(line)
-        else:
-            seen.add(line_lower)
+with open('hw11_miniproj_data1.txt', 'r+') as f:
+    d = f.readlines()
+    f.seek(0)
+    for i in d:
+        if i not in lines_seen:
+            f.write(i)
+            print(i)
+            lines_seen.add(i)
+    f.truncate()
+    
+    num_lines = sum(1 for line in open('hw11_miniproj_data1.txt')) - 1
+    print("Number of people = " + str(num_lines))
+    f.close()
+    
+# if you find 2, then remove it from the file
+
+# number of non-duplicates in new file + number of lines in duplicate list = total number of people
+
+# when we find the line in the file, and that matches the line in the set
+
